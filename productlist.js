fbc91b7c6c11d286c439bf107226ba89310625c8
Frederik <frha1004@stud.ek.dk>
2026-02-27 11:33:12 +0100

    Dynamiske produkter

--- productlist.js
+++ productlist.js
@@ -1,6 +1,11 @@
 console.log("Det virker");
+
+const kategori = new URLSearchParams(window.location.search).get("kat");
+console.log(kategori);
+
 const container = document.querySelector(".product-grid");
-const endpoint = "https://kea-alt-del.dk/t7/api/products";
+const endpoint = `https://kea-alt-del.dk/t7/api/products?category=${kategori}`;
+document.querySelector("h1").textContent = kategori;
 
 function getdata() {
   fetch(endpoint)
@@ -10,26 +15,28 @@ function getdata() {
 function showdata(data) {
   let markup = "";
   console.log(data);
-  data.forEach((element) => console.log(element));
-  markup += ` <article class="product-card">
+  data.forEach((element) => {
+    console.log(element);
+    markup += ` <article class="product-card">
           <a href="product.html">
             <div class="image-wrapper">
               <span class="sold-label">Udsolgt</span>
               <img
-                src="https://kea-alt-del.dk/t7/images/webp/640/1165.webp"
+                src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp"
                 alt="Cardigan"
               />
             </div>
 
-            <h2>Sort Cardigan</h2>
+            <h2>${element.productdisplayname}</h2>
             <p class="brand">Adidas</p>
 
             <div class="discount">
-              <p class="price">149 kr</p>
-              <span class="offer-label">Tilbud, 100 kr</span>
+              <p class="price">${element.price} Kr.</p>
+              <span class="offer-label">${element.discount}</span>
             </div>
           </a>
         </article>`;
+  });
 
   container.innerHTML = markup;
 }
