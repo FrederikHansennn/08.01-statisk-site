ca0eaa257fe513da0437dfd4a7aeb1d230733694
Frederik <frha1004@stud.ek.dk>
2026-03-02 12:36:07 +0100

    Dynamisk

--- productlist.js
+++ productlist.js
@@ -1,10 +1,14 @@
 console.log("Det virker");
 
+// Henter kategori fra URL'en (f.eks. ?kat=skjorter)
 const kategori = new URLSearchParams(window.location.search).get("kat");
 console.log(kategori);
 
+// Finder containeren i HTML'en
 const container = document.querySelector(".product-grid");
+// Bygger linket til API'et med den valgte kategori
 const endpoint = `https://kea-alt-del.dk/t7/api/products?category=${kategori}`;
+// Sætter overskriften på siden til at være kategoriens navn
 document.querySelector("h1").textContent = kategori;
 
 function getdata() {
@@ -12,11 +16,14 @@ function getdata() {
     .then((respons) => respons.json())
     .then(showdata);
 }
+
 function showdata(data) {
   let markup = "";
   console.log(data);
+
   data.forEach((element) => {
     console.log(element);
+    // Her bygges HTML'en uden at tjekke for discount eller udsolgt
     markup += ` <article class="product-card">
           <a href="product.html">
             <div class="image-wrapper">
@@ -38,7 +45,9 @@ function showdata(data) {
         </article>`;
   });
 
+  // Indsætter al HTML i containeren på én gang
   container.innerHTML = markup;
 }
 
+// Starter hele processen
 getdata();
